b01e95d0295b61b05ae8cb52e4d05bb545ca680b
Phát Trịnh Tấn <trinhtanphat2403@gmail.com>
2026-03-02 10:37:19 +0700

    ci: add WHMCS/Capsule stubs for PHPStan symbol discovery

---
+++ stubs/whmcs-phpstan-stubs.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Illuminate\Database\Capsule {
+    class Manager
+    {
+        public static function schema()
+        {
+        }
+
+        public static function table($table)
+        {
+        }
+
+        public static function raw($expression)
+        {
+        }
+    }
+}
+
+namespace {
+    function check_token($tokenName = null)
+    {
+    }
+
+    function generate_token($type = null)
+    {
+        return '';
+    }
+
+    function add_hook($hookName, $priority, $function)
+    {
+    }
+}
